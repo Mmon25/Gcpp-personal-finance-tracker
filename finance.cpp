4e524c88619725d5f7e00928a84dedcd9745c7eb
Matthew Montecillo <montemat15@gmail.com>
2026-04-21 11:50:28 -0700

    Refactor project into multi-file structure (main.cpp, finance.cpp, finance.h)

---
+++ finance.cpp
@@ -0,0 +1,29 @@
+// finance.cpp
+// Contains function definitions for finance.h
+// Contains function implementations for addIncome, addExpense, and displayBalance
+#include <iostream>
+#include <iomanip>
+#include "finance.h" // Inclue header to access function declarations
+
+using namespace std;
+
+// userBal passed by reference so functions can modify original value
+
+    void addIncome(double newIncome, double &userBal){ // newIncome passed by value, userBal by reference to update original value
+        
+        userBal += newIncome; // Apply new income to update balance value
+
+        cout << "\n";
+    }
+
+    void addExpense(double newExpense, double &userBal){ // newExpense passed by value, userBal by reference to update original value
+        
+        userBal -= newExpense; // Apply new expense to update balance value
+
+        cout << "\n";
+    }
+
+    void displayBalance(double userBal){ // userBal passed by value since this function does not require modification of original value
+
+        cout << "\n\tYour current balance: $" << userBal << endl; // Output balance
+    }
