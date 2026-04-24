fb84752c261da80ed08785943f17e486afe9490c
Matthew Montecillo <montemat15@gmail.com>
2026-04-23 19:24:57 -0700

    Delete "using namespace std;"
    Updated function prototypes to use std::vector and std::string.

--- finance.h
+++ finance.h
@@ -5,13 +5,11 @@
 #include <vector>
 #include <string>
 
-using namespace std;
-
 // finance.h
 // Declared finance-related functions for use in other files
-void addIncome(double newIncome, double &userBal, vector<string> &history); // addIncome function prototype
-void addExpense(double newExpense, double &userBal, vector<string> &history); // addExpense function prototype
+void addIncome(double newIncome, double &userBal, std::vector<std::string> &history); // addIncome function prototype
+void addExpense(double newExpense, double &userBal, std::vector<std::string> &history); // addExpense function prototype
 void displayBalance(double userBal); // displayBalance function prototype
-void displayHistory(const vector<string> &history); // Transaction prototype
+void displayHistory(const std::vector<std::string> &history); // Transaction prototype
 
 #endif
